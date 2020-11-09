fd5ae4ea2664317a262c65f85b71652d3226b8a0
Nayoung-apeach <s2019w04@e-mirim.hs.kr>
2020-11-09 20:26:09 +0900

    Init WebViewActivity :D

---
+++ app/src/main/java/kr/hs/emirim/s2019w04/mirimtabhost/WebViewActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2019w04.mirimtabhost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
